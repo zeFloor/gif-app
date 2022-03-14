733299850dc951f4f4a392b34fc19cea4ca6fe3e
zeFloor <erviss1@gmail.com>
2022-03-15 00:00:16 +0200

    natural language API implementation

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,29 @@
+import axios from 'axios'
+import { useState } from 'react';
 
 function App() {
-  return <></>
+  const [ inputText, setInputText ] = useState()
+  console.log(inputText)
+
+  const content = {
+    method: "post",
+    url: "http://localhost:5000/input",
+    data: {
+      text: `${inputText}`
+    }
+  }
+
+  const submitInput = () => {
+    axios(content)
+      .catch(err => console.log(err))
+  }
+
+  return (
+    <form>
+      <input type="text" onChange={e => setInputText(e.target.value)} />
+      <button onClick={submitInput}>Search</button>
+    </form>
+  )
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,55 @@
 const express = require('express')
-const cros = require('cros')
+const cors = require('cors')
 const axios = require('axios')
+const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1')
+const { IamAuthenticator } = require('ibm-watson/auth')
 
 const app = express()
 
-app.use(cros())
+app.use(cors())
 app.use(express.json())
+
+let inputText = ''
+let searchText = ''
+
+app.post('/input', (req, res) => {
+
+    inputText = req.body.text
+    if(inputText.split(' ').length < 3) {
+        searchText = inputText
+    } else {
+        console.log(inputText)
+        const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+            
+            version: '2021-08-01',
+            authenticator: new IamAuthenticator({
+              apikey: '4allRA2o-g2Hj3cdV9qiLvvWulR6AVLKfY-LruG3j2HQ',
+            }),
+            serviceUrl: 'https://api.eu-gb.natural-language-understanding.watson.cloud.ibm.com/instances/ac38bb67-dedb-46c9-b1c9-cca899b355f1',
+          })
+          
+          let analyzeParams = {
+            'text': `${inputText}`,
+            'features': {
+              'concepts': {
+                'limit': 3
+              }
+            }
+          }
+          
+          naturalLanguageUnderstanding.analyze(analyzeParams)
+            .then(analysisResults => {
+              console.log(analysisResults.result.concepts[0].text)
+              searchText = analysisResults.result.concepts[0].text
+            })
+            .catch(err => {
+                console.log("Error:", err)
+            })       
+    }
+    
+})
+
+
+
+
+app.listen(5000, console.log("Listening on port 5000"))
